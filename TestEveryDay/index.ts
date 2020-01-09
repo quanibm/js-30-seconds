07c52bb188e604c92bd552121d46b2d6d396bc60
zat.圈 <quan@qq.com>
2020-01-09 18:01:36 +0800

    插入排序

--- TestEveryDay/index.ts
+++ TestEveryDay/index.ts
@@ -39,9 +39,10 @@ const fn = arr => {
 //   fn(arr)
 // )
 // fn(arr) => [{id: 1, child: [{id: 2, pId: 1, child: [{ id: 3, pId: 2}]}]}, {id: 4, child: [{id: 5, pId: 4}]}]
-interface Array<T>{
-        bubbleSort(): any
-        selectionSort(): any
+interface Array<T> {
+  bubbleSort(): any;
+  selectionSort(): any;
+  insertionSort(): any;
 }
 
 /**
@@ -58,45 +59,66 @@ Array.prototype.bubbleSort = function() {
     }
   }
 };
-// let arr = [1,2,3,333,4422,1,134,4,4314,143,14,14,3134,1,4134,1,4312341,2414321,414]
-// arr.bubbleSort()
-// console.log(arr)
+// let arr = [1, 7, 6, 2, 4, 3, 9];
+// console.log(arr.bubbleSort());
 
 Array.prototype.selectionSort = function() {
-  let index;
-  for (let i = 0, len = this.length - 1; i < len; i++) {
-    index = i;
-    for (let j = i, len1 = this.length; j < len1; j++) {
-      if(this[index] > this[j])
+  const len = this.length;
+  let indexMin
+  for (let i = 0; i < len - 1; i++)
+  {
+    indexMin = i
+    for (let j = i + 1; j < len; j++)
+    { 
+      if(this[indexMin] > this[j])
       {
-        index = j;
+        indexMin = j
       }
-      console.log('index', index, this[index], this[j])
+    }
+     if (indexMin != i) {
+       const aux = this[i];
+       this[i] = this[indexMin];
+       this[indexMin] = aux;
+       console.log(this)
+     }
+  }
+  return this
+};
+// let arr = [1, 7, 6, 2, 4, 3, 9];
+// let arr = [1, 7, 6, 2, 4,32,32,3,112,1,21,3,2,32,3,2,32,4,24,2,31,3,1,3,13,1,3,2,3,23,2,3,231,2,13, 3, 9];
+// console.log(arr.selectionSort());
 
+/**
+ * 思想
+    一般人打扑克牌，整理牌的时候，都是按牌的大小（从小到大或者从大到小）整理牌的，那每摸一张新牌，就扫描自己的牌，把新牌插入到相应的位置。
+    插入排序的工作原理：通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+
+ * 步骤
+    1.从第一个元素开始，该元素可以认为已经被排序；
+    2.取出下一个元素，在已经排序的元素序列中从后向前扫描；
+    3.如果该元素（已排序）大于新元素，将该元素移到下一位置；
+    4.重复步骤 3，直到找到已排序的元素小于或者等于新元素的位置；
+    5.将新元素插入到该位置后；
+    6.重复步骤 2~5。
+
+ */
+Array.prototype.insertionSort = function() {
+  let len = this.length;
+  let preIndex, current;
+  for (let i = 0;  i < len; i++)
+  {
+    preIndex = i - 1; 
+    current = this[i]
+    while (preIndex >= 0 && this[preIndex] > current)
+    {
+      this[preIndex + 1] = this[preIndex];
+      preIndex--;
+    }
+    if(preIndex + 1 != i) {
+      this[preIndex + 1] = current;
     }
   }
+  return this
 };
-
-let arr = [
-  1,
-  2,
-  3,
-  333,
-  4422,
-  1,
-  134,
-  4,
-  4314,
-  143,
-  14,
-  14,
-  3134,
-  1,
-  4134,
-  1,
-  4312341,
-  2414321,
-  414
-];
-arr.selectionSort();
-console.log(arr);
+// let arr = [1, 7, 6, 2, 4,32,32,3,112,1,21,3,2,32,3,2,32,4,24,2,31,3,1,3,13,1,3,2,3,23,2,3,231,2,13, 3, 9];
+// console.log(arr.insertionSort());
